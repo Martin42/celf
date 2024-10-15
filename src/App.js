ae7ed69fa73fb14fd5656ee7d0d4dc6816258238
Martin42 <martinarmada@ua.pt>
2024-10-15 13:57:45 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <header>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.celfcentrodeformacao.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          WIP for CELF
         </a>
       </header>
     </div>
